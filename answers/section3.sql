ecf8985a39c0b8235667c31e70f93fed24c22da0
k-romero <imkevinromero@gmail.com>
2020-03-10 21:54:49 -0400

    completed part 2 and 3

--- answers/section3.sql
+++ answers/section3.sql
@@ -1,1 +1,42 @@
-SELECT * FROM pokemons;
+/*What is each pokemon's primary type?*/
+SELECT pokemons.name, types.name
+FROM pokemons, types
+WHERE pokemons.primary_type = types.id
+ORDER BY types.name,pokemons.name;
+
+/*What is Rufflet's secondary type?*/
+SELECT pokemons.name, types.name
+FROM pokemons, types
+WHERE pokemons.name = 'Rufflet' AND pokemons.secondary_type = types.id;
+
+/*What are the names of the pokemon that belong to the trainer with trainerID 303?*/
+SELECT pokemons.name
+FROM pokemon_trainer, pokemons
+WHERE pokemon_trainer.trainerid = 303 AND pokemon_trainer.pokemon_id = pokemons.id
+ORDER BY name;
+
+/*How many pokemon have a secondary type Poison*/
+SELECT COUNT (*)
+FROM pokemons, types
+WHERE pokemons.secondary_type = types.id AND types.name = 'Poison';
+
+/*What are all the primary types and how many pokemon have that type?*/
+SELECT DISTINCT pokemons.primary_type, COUNT(pokemons.name)
+FROM types, pokemons
+WHERE types.id = pokemons.primary_type
+GROUP BY pokemons.primary_type;
+
+/*How many pokemon at level 100 does each trainer with at least one level 100 pokemone have?
+Hint: your query should not display a trainer*/
+SELECT pokemons.name, pokemon_trainer.pokelevel, COUNT(*)
+FROM pokemon_trainer, pokemons
+WHERE pokemon_trainer.pokemon_id = pokemons.id AND pokelevel = 100
+GROUP BY pokemons.name, pokemon_trainer.pokelevel
+ORDER BY COUNT,pokemons.name;
+
+/*How many pokemon only belong to one trainer and no other?*/
+SELECT pokemons.name, COUNT (*)
+FROM pokemons, pokemon_trainer, trainers
+WHERE pokemons.id = pokemon_trainer.pokemon_id AND pokemon_trainer.trainerid = trainers.trainerid
+GROUP BY pokemons.name
+HAVING COUNT(*) = 1;
--- answers/section4.sql
+++ answers/section4.sql
@@ -1,1 +1,13 @@
-SELECT * FROM pokemons;
+/*
+Directions: Write a query that returns the following columns:
+-------------------------------------------------------------------------------
+Pokemon Name:Pokemon's name
+Trainer Name:Trainer's name
+Level:Current level
+Primary Type:Primary Type Name
+Secondary Type:Secondary Type Name
+-------------------------------------------------------------------------------
+Sort the data by finding out which trainer has the strongest pokemon so that
+this will act as a ranking of strongest to weakest trainer. You may interpret 
+strongest in whatever way you want, but you will have to explain your decision.
+*/
